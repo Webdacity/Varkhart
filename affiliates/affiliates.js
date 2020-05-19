ecbb4addf26116f241ad8d78c6bf8b2959797c4a
Arno van Staden <arnovanstaden@gmail.com>
2020-05-19 14:26:12 +0200

    update

--- affiliates/affiliates.js
+++ affiliates/affiliates.js
@@ -1,24 +1,4 @@
 // Get Affiliate Code
-const url = window.location.href;
-if (url.includes("?")) {
-    const affiliateCode = url.substring(url.indexOf("?") + 1, url.length);
-
-    // Check if Affiliate Active
-    if (checkAffiliateActive(affiliateCode)) {
-        var fortnightAway = new Date(Date.now() + 12096e5);
-        console.log(fortnightAway);
-        document.cookie = `afflCode=${affiliateCode};expires=${fortnightAway.toGMTString()};path=/`;
-
-        window.location.replace("../index.html");
-    } else {
-        window.location.replace("../index.html");
-    }
-
-}
-//  If No Code in url
-else {
-    window.location.replace("../index.html");
-}
 
 const checkAffiliateActive = (affiliateCode) => {
     axios({
@@ -35,4 +15,27 @@ const checkAffiliateActive = (affiliateCode) => {
         .catch(error => {
             console.log(error)
         })
+}
+
+const url = window.location.href;
+if (url.includes("?")) {
+    const affiliateCode = url.substring(url.indexOf("?") + 1, url.length);
+
+    // Check if cookie already exists
+
+    // Check if Affiliate Active
+    if (checkAffiliateActive(affiliateCode)) {
+        var fortnightAway = new Date(Date.now() + 12096e5);
+        console.log(fortnightAway);
+        document.cookie = `afflCode=${affiliateCode};expires=${fortnightAway.toGMTString()};path=/`;
+
+        window.location.replace("../index.html");
+    } else {
+        window.location.replace("../index.html");
+    }
+
+}
+//  If No Code in url
+else {
+    window.location.replace("../index.html");
 }
